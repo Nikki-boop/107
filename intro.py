80858e9ae739a7262912a654dfbfa26b9f03f522
Nikki-boop <bogdnico@gmail>
2024-10-04 22:06:08 -0500

    Ass. 1 107

---
+++ intro.py
@@ -0,0 +1,34 @@
+print("Hello world")
+# console.log("Hello world")
+
+# Variables-you dont have to define or specify what type of variable
+name = "nicole"
+age = 138
+found = False
+name = True
+name = 12
+# let name = "nicole"
+# const = "nicole"
+print(name)
+
+#if/else statement
+if age<100:
+    print ("don't worry you're not that old")
+    #this is on the same level
+    # also on the same level
+elif age==100:
+    print("wow youre a century")
+else:
+    print("well seems tha you're a little bit old")
+
+#functions
+def say_Hello():
+    print("Hello there")
+#function say_Hello(){}
+
+#call functions
+say_Hello()
+
+print("my age is "+str(age)+" years old")
+
+
